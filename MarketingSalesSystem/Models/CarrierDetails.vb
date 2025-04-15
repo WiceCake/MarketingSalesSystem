076a6a4fd89a3afa4833cd547fe71192b3672f2e
Kian <73981893+kiangraphy@users.noreply.github.com>
2025-04-15 08:59:06 +0800

    update

--- MarketingSalesSystem/Models/CarrierDetails.vb
+++ MarketingSalesSystem/Models/CarrierDetails.vb
@@ -49,7 +49,7 @@
             .UnloadedValue = UnloadedValue
             .DateCreated = DateCreated
         End With
-
+        'Hello
 
         dc.trans_SalesUnloadeds.InsertOnSubmit(carrier)
         dc.SubmitChanges()
